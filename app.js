238b5a231b82804d2384d339ff20ba5e0085711c
hypercubist <qpinguz@gmail.com>
2020-03-27 00:35:34 +0900

    200327

---
+++ app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import routes from "./routes.js";
+
+const app = express();
+
+app.use(helmet());
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.video, videoRouter);
+
+export default app;
